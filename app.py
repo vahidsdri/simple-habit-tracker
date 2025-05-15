fff15fcc533aeac96ba508483de0849f8dc5e527
Vahid Sadri <vsdriuc@gmail.com>
2025-05-15 19:24:20 +0330

    add styling for callendar

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 import datetime
+from collections import defaultdict
 from flask import Flask, render_template, request
 app = Flask(__name__)
+
+
 habits = ["wash clothes"]
+completions = defaultdict(list)
+
 
 @app.context_processor
 def add_clac_date_range():
@@ -9,6 +14,8 @@ def add_clac_date_range():
         dates = [start + datetime.timedelta(days=diff) for diff in range(-3, 4)]
         return dates
     return {"date_range": date_range}
+
+
 @app.route('/')
 def index():
     date_str = request.args.get("date")
@@ -16,7 +23,7 @@ def index():
         selected_date = datetime.date.fromisoformat(date_str)
     else:
         selected_date = datetime.date.today()
-    return render_template('index.html', title="Habit Tracker - Home", habits=habits, selected_date=selected_date)
+    return render_template('index.html', title="Habit Tracker - Home", habits=habits, selected_date=selected_date,completions=completions[selected_date])
 
 
 @app.route('/add',methods=["GET","POST"])
